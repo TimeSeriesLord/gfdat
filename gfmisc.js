a4698e23b457afb99e33e30628159e2b641b2250
TimeSeriesLord <fearless.smarty@gmail.com>
2017-11-30 00:21:55 -0500

    Add files via upload

--- gfmisc.js
+++ gfmisc.js
@@ -3,11 +3,11 @@ function toTitleCase(str)
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-function properCounty (condition) {
-  if (typeof countiesLookup[condition] !== 'function') {
-    return toTitleCase(condition);
+function properCounty (county) {
+  if (typeof countiesLookup[county] !== 'function') {
+    return toTitleCase(county);
   }
-  return countiesLookup[condition]();
+  return countiesLookup[county]();
 }
 
 
